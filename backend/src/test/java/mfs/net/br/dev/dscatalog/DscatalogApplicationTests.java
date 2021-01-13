e32538698d8a100738742c607538c15ceb5e83b0
marcelinoSousa <marcelino.feliciano@hotmail.com>
2021-01-13 13:39:50 -0400

    Project created

---
+++ backend/src/test/java/mfs/net/br/dev/dscatalog/DscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package mfs.net.br.dev.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
