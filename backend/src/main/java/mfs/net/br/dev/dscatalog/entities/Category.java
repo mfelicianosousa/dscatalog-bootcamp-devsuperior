0516fd23deca330c147e0f1d7c0693060ca0ffff
marcelinoSousa <marcelino.feliciano@hotmail.com>
2021-01-19 22:26:15 -0400

    Product entity, SQL script

--- backend/src/main/java/mfs/net/br/dev/dscatalog/entities/Category.java
+++ backend/src/main/java/mfs/net/br/dev/dscatalog/entities/Category.java
@@ -15,10 +15,7 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "category")
 public class Category implements Serializable {
-	
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 1L;
 	
 	@Id
