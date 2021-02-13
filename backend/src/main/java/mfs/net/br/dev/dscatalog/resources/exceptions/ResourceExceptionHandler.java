e263cad28dc91ca4fbd2c9eee99f259f98151575
marcelinoSousa <marcelino.feliciano@hotmail.com>
2021-02-13 09:52:57 -0400

    Custom validation response

--- backend/src/main/java/mfs/net/br/dev/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/mfs/net/br/dev/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -38,6 +40,25 @@ public class ResourceExceptionHandler {
 		err.setError("Database exception");
 		err.setMessage(e.getMessage());
 		err.setPath(request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(err);
+		
+	}
+	
+	@ExceptionHandler( MethodArgumentNotValidException.class)
+	public ResponseEntity< ValidationError > Validation( MethodArgumentNotValidException e, HttpServletRequest request){
+		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY ;
+		ValidationError err = new ValidationError();
+		err.setTimeStamp(Instant.now());
+		err.setStatus( status.value() );
+		err.setError("Validation exception");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		
+		for (FieldError fieldError : e.getBindingResult().getFieldErrors()) {
+			// Adiciona o erro na lista de erros personalizados
+			err.AddError(fieldError.getField(), fieldError.getDefaultMessage());
+		}
 		
 		return ResponseEntity.status(status).body(err);
 		
---
+++ backend/src/main/java/mfs/net/br/dev/dscatalog/resources/exceptions/ValidationError.java
@@ -0,0 +1,22 @@
+package mfs.net.br.dev.dscatalog.resources.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError {
+	
+	private static final long serialVersionUID = 1L;
+
+	private List<FieldMessage> errors = new ArrayList<>();
+
+	public List<FieldMessage> getErrors() {
+		return errors;
+	}
+	
+	public void AddError( String fieldName, String message ) {
+		
+		errors.add(new FieldMessage(fieldName, message));
+	}
+	
+
+}
