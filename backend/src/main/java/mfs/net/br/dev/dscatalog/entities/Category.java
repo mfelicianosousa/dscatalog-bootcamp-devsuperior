65c1b961120544a58fd930c4a69c2a3d532e8e29
marcelinoSousa <marcelino.feliciano@hotmail.com>
2021-02-12 22:43:43 -0400

    ORM mapping, database seeding

--- backend/src/main/java/mfs/net/br/dev/dscatalog/entities/Category.java
+++ backend/src/main/java/mfs/net/br/dev/dscatalog/entities/Category.java
@@ -2,12 +2,15 @@ package mfs.net.br.dev.dscatalog.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
@@ -29,7 +32,14 @@ public class Category implements Serializable {
 	@Column(columnDefinition="TIMESTAMP WITHOUT TIME ZONE")
 	private Instant updatedAt ;
 	
+	@ManyToMany(mappedBy = "categories")
+	private Set<Product> products = new HashSet<>();
 	
+	
+	public Set<Product> getProducts() {
+		return products;
+	}
+
 	public Category() {
 		
 	}
